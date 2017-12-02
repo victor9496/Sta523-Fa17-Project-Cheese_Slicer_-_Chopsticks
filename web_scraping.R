0f2f8f76753407d553e33882b3e918e654727e2c
victor lee <victor9496@gmail.com>
2017-12-02 17:54:47 -0500

    typo change and include averge area

--- web_scraping.R
+++ web_scraping.R
@@ -35,8 +35,7 @@ review_score = extract_info('script[type="application/ld+json"]') %>%
 lon_lat = site %>%
   read_html() %>%
   html_text() %>% 
-  str_extract_all("\\[\\'longitude\\'\\] = '-\\d+\\.\\d+|
-                  \\[\\'latitude\\'\\] = '\\d+\\.\\d+") %>%  
+  str_extract_all("\\[\\'longitude\\'\\] = '-\\d+\\.\\d+|\\[\\'latitude\\'\\] = '\\d+\\.\\d+") %>%  
   str_extract_all("-?\\d+\\.\\d+") %>% 
   unlist() %>% 
   as.numeric()
@@ -44,12 +43,11 @@ lon_lat = site %>%
 #apartment name
 apt_name = extract_info('.last span')
 
-#floor plan
+#floor plan(change)
 floor_plan = site %>%
   read_html() %>%
-  html_nodes('a[href="#floorplans"]') %>% 
-  html_text() %>% 
-  .[str_detect(., "\\d")]
+  html_nodes('h3[class="link1"]') %>% 
+  html_text()
 
 #rent
 rent = site %>%
@@ -70,14 +68,31 @@ image_url = site %>%
 
 #distance to chapel
 chapel = c(-78.9424706, 36.0018988)
-distance = round(distm(lon_lat, chapel, fun = distHaversine)[1])
+distance = round(distm(lon_lat, chapel, fun = distHaversine)[1]) %>% 
+  as.numeric()
 
-df.final =  as.data.frame(cbind(apt_name, review_score, distance, image_url), 
+floor = site %>%
+  read_html() %>%
+  html_nodes('#floorplans .widget') %>% 
+  html_text()
+
+split_floor = unlist(str_split(floor, "\\d Bedrooms, \\d Bathroom"), recursive = FALSE)
+
+data = unlist(lapply(split_floor, 
+                     function(i) str_extract_all(str_replace_all(i, ",", ""), "\\d{3,}")), 
+              recursive = FALSE)
+
+floor_mean = unlist(lapply(data, function(i) mean(as.numeric(i))))
+floor_mean_clean = floor_mean[!is.nan(floor_mean)]
+
+#generate final dataframe
+df.final =  as.data.frame(cbind(apt_name, image_url), 
                           stringsAsFactors = FALSE) %>% 
   slice(rep(1:n(), length(floor_plan))) %>% 
-  cbind(floor_plan, rent)
+  cbind(floor_plan, rent, floor_mean_clean, review_score, distance)
 
-colnames(df.final) = c("name", "count", "score", "distance", "image", "plan", "rent")
+colnames(df.final) = c("name", "image", "plan", "rent", 
+                       "size", "review_count", "avg_review","distance")
 
 #different score
 
@@ -85,7 +100,6 @@ colnames(df.final) = c("name", "count", "score", "distance", "image", "plan", "r
 #   read_html() %>%
 #   html_nodes('#content_PropertyBreakdown .score') %>% 
 #   html_text() 
-
 
 return(df.final)
 
